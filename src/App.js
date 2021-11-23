13552668c42f634d6721f2b1c493a56202bd2467
JESSE ARROYO-ROMERO <jesseromero310@gmail.com>
2021-11-23 15:46:54 -0600

    Created table for products

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import HeaderAndNav from './components/HeaderAndNav/HeaderAndNav'
 import HomePage from './components/HomePage/HomePage';
 import ProductTable from './components/ProductTable/ProductTable'
 import SellProductTable from './components/SellProductTable/SellProductTable'
+import axios from 'axios'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
+function App() {
+
+  const [productId, setProductId] = useState([])
+  const [productList, setProductList] = useState([])
+
+  useEffect(() =>{
+
+  },[productId])
+
+  async function getProducts() {
+    let response = await axios.get('https://localhost:44394/api/Products')
+    setProductId(response.data.Name)
   }
-  render() { 
+
+  render() 
     return (
     <div>
       <Router>
         <HeaderAndNav />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductTable />} />
+          <Route path="/products" element={<ProductTable />} getProducts={getProducts}/>
           <Route path="/sellProducts" element={<SellProductTable />} />
         </Routes>
       </Router>
       
     </div>  );
   }
-}
+
  
 export default App
 ;
--- src/components/ProductTable/ProductTable.jsx
+++ src/components/ProductTable/ProductTable.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 
 const ProductTable = (props) => {
-    return (
-        <h1>Hello</h1>
-      );
+  
+  <React.Fragment>
+    <div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <div>
+        {props.getProducts.map(function(product){
+          return (
+              <tr>
+                <td>{product.id}</td>
+                <td>{product.Name}</td>
+                <td>{product.Description}</td>
+                <td>{product.Price}</td>
+              </tr>
+            )
+        })}
+      </div>
+      </table>
+    </div>
+  </React.Fragment>
 }
  
 export default ProductTable;
