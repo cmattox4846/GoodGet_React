94dd38974331c11f492ba36af8e52f9611f450a6
Jacob Haustveit <jacobhaustveit@gmail.com>
2021-11-24 13:06:45 -0500

    Updated Form styling in user registration file.

--- src/components/UserRegistration/UserRegistration.jsx
+++ src/components/UserRegistration/UserRegistration.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
 
 class UserRegistration extends Component {
     constructor(props){
@@ -27,21 +29,40 @@ class UserRegistration extends Component {
     render() {
         return (
             <div className="formCreate">
-                <form onSubmit={this.handleSubmit}>
-                    <label>First Name:</label>
-                    <input type="text" name='firstName' onChange={this.handleChange} value={this.state.firstName}/>
-                    <label>Last Name:</label>
-                    <input type="text" name='lastName' onChange={this.handleChange} value={this.state.lastName}/>
-                    <label>Username:</label>
-                    <input type="text" name='userName' onChange={this.handleChange} value={this.state.userName}/>
-                    <label>Email:</label>
-                    <input type="text" name='email' onChange={this.handleChange} value={this.state.email}/>
-                    <label>Password:</label>
-                    <input type="text" name='password' onChange={this.handleChange} value={this.state.password}/>
-                    <label>PhoneNumber:</label>
-                    <input type="text" name='phoneNumber' onChange={this.handleChange} value={this.state.phoneNumber}/>
-                    <button className="button" type="submit">Create User</button>
-                </form>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>First Name:</Form.Label>
+                        <Form.Control type="text" name='firstName' onChange={this.handleChange} value={this.state.firstName}/>
+                    </Form.Group>
+                     
+                    <Form.Group>
+                        <Form.Label>Last Name:</Form.Label>
+                        <Form.Control type="text" name='lastName' onChange={this.handleChange} value={this.state.lastName}/>
+                    </Form.Group>
+
+                                            
+                    <Form.Group>
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control type="text" name='userName' onChange={this.handleChange} value={this.state.userName}/>
+                    </Form.Group>
+                    
+                    <Form.Group>
+                        <Form.Label>Email:</Form.Label>
+                        <Form.Control type="email" name='email' onChange={this.handleChange} value={this.state.email}/>
+                    </Form.Group>
+                    
+                    <Form.Group>
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type="password" name='password' onChange={this.handleChange} value={this.state.password}/>
+                    </Form.Group>
+                    
+                    <Form.Group>
+                        <Form.Label>PhoneNumber:</Form.Label>
+                        <Form.Control type="text" name='phoneNumber' onChange={this.handleChange} value={this.state.phoneNumber}/>
+                    </Form.Group>
+                   
+                    <Button className="button" type="submit">Create User</Button>
+                </Form>
             </div>
         )
     }
