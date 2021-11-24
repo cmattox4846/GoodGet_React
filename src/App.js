bd34609d360a80dc335661290171c686744a29d3
Jacob Haustveit <jacobhaustveit@gmail.com>
2021-11-24 12:28:47 -0500

    Created method to login in user and get access to user token.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState} from 'react';
+// import jwtDecode from 'jwt-decode;'
+import jwtDecode from 'jwt-decode';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import HeaderAndNav from './components/HeaderAndNav/HeaderAndNav';
@@ -12,25 +14,57 @@ function App() {
 
   const [productId, setProductId] = useState([])
   const [productList, setProductList] = useState([])
+  const [user, setUser] = useState("")
+  const [userLogin, setUserLogin] = useState([])
+
+  const getUserJWT = () => {
+    const jwt = localStorage.getItem('token');
+    try {
+      const user = jwtDecode(jwt);
+      setUser(user)
+      console.log(user)
+    } catch (error) {
+      console.log("Error in decoding JWT token: ", error)
+    }
+  }
 
   useEffect(() =>{
+    getUserLogin();
+  },[])
 
+  useEffect(() =>{
+    getProducts()
   },[productId])
 
+  // Get user login
+  const getUserLogin = async () => {
+    const jwt = localStorage.getItem('token');
+    const response = await axios.get('https://localhost:44394/api/authentication/user', { headers: {Authorization: 'Bearer ' + jwt}});
+    setUserLogin(userLogin);
+  }
+
+  // Get Product info on id change
   async function getProducts() {
     let response = await axios.get('https://localhost:44394/api/Products')
     setProductId(response.data.Name)
   }
 
+  // * CAll back functions
+  const loginUser = async (loginUser) => {
+    let response= await axios.post('https://localhost:44394/api/authentication/login', loginUser);
+    localStorage.setItem('token', response.data.token);
+    console.log(response.data.token)
+    
+  }
   
     return (
       <div>
         <Router>
           <HeaderAndNav />
           <Routes>
-            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/login" element={<LoginScreen loginUserCall={loginUser} />} />
             <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductTable />} />
+            <Route path="/products" element={<ProductTable />} />           
             <Route path="/sellProducts" element={<SellProductTable />} />
           </Routes>
         </Router>
