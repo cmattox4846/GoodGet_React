ca1a1107bd57164ba4779fba7e53ccefe7136efd
Jacob Haustveit <jacobhaustveit@gmail.com>
2021-11-29 11:45:03 -0500

    added Header to product table

--- src/components/ProductTable/ProductTable.jsx
+++ src/components/ProductTable/ProductTable.jsx
@@ -1,27 +1,34 @@
 import React from "react";
-import Table from 'react-bootstrap/Table'
+import Table from "react-bootstrap/Table";
 
 const ProductTable = (props) => {
-    return (
-        props.listOfProducts.map(function(product){
-          return (
-              <div key={product.id}>
-                <Table>                    
-                    <tbody>
-                        <tr>
-                             <td>{product.id}</td>
-                             <td>{product.name}</td>
-                             <td>{product.description}</td>
-                             <td>{product.price}</td>
-                        </tr>
-                    </tbody>    
-                </Table>
-            </div>
-            )
-          }
-        )
-    )
-  
-}
- 
+  return (
+    <div>
+      <Table>
+        <thead>
+            <tr>
+                <td>Product ID</td>
+                <td>Name</td>
+                <td>Description</td>
+                <td>Price</td>
+
+            </tr>
+        </thead>
+        <tbody>
+          {props.listOfProducts.map((product) => {
+            return (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+              </tr>              
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
 export default ProductTable;
