627b3f952b318a244aaead126b630fc3df4b8ea1
Jacob Haustveit <jacobhaustveit@gmail.com>
2021-11-30 13:44:07 -0500

    Updated login screen to redirect user to profile page after logging in

--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -7,13 +7,15 @@ import useForm from './useForm';
 
 
 const LoginScreen = (props) => {
-    const {formValues, handleChange, handleSubmit } = useForm(register);
+    const {formValues, handleChange, handleSubmit } = useForm(Register);
     
-    function register() {
-        props.loginUserCall()
-        alert('Thanks for Registering!')
-        
+    let navigate= useNavigate();
+
+    async function Register() {                   
+        props.loginUserCall(formValues)
+        navigate("../Profile")
     }
+   
 
 
     return ( 
@@ -21,16 +23,16 @@ const LoginScreen = (props) => {
             <Form onSubmit= {handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>User Name</Form.Label>
-                  <Form.Control type="username" name="username" placeholder="Enter UserName" onChange= {handleChange} value= {formValues.username} required= {true}/>
+                  <Form.Control type="username" name="username" placeholder="Enter UserName" onChange= {handleChange} required= {true}/>
 
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" name="password" placeholder="Password" onChange= {handleChange} value= {formValues.password} required= {true}/>
+                  <Form.Control type="password" name="password" placeholder="Password" onChange= {handleChange} required= {true}/>
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" >
                   Submit
                 </Button>
             </Form>
--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,44 +1,69 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import useForm from "../LoginScreen/useForm";
+import StarRating from 'react-bootstrap-star-rating';
 
-
-
-
-// const ProductDetails = (product) => {
-//     return (
-//         <div>
-//             <Table>
-//                 <tbody>
-//                 <tr>
-//                 <td>{product.id}</td>
-//                 <td>{product.name}</td>
-//                 <td>{product.description}</td>
-//                 <td>{product.price}</td>
-//                 <td>{product.reveiw}</td>
-//                 <td><button onClick={() => add(product)}>Add Product</button></td></tr>
-//                 </tbody>
-//             </Table>
-//         </div>
-//     )
-// }
-// export default ProductDetails
 
 
 const DetailTable = (props) => {
+
+  const {formValues, handleChange, handleSubmit } = useForm(addReview);
+  
+  function addReview() {
+      try{
+          props.addReview(formValues)
+      
+      } catch (error) {
+          console.error(error.message)
+      }
+      
+      
+  }
+  
+
   return (
     <div>
-  
-      
-          <Table>
-            <tbody>
-            <tr><td>{props.details.name}</td></tr>
-            <tr><td>{props.details.description}</td></tr>
-            <tr><td>{props.details.price}</td></tr>
-            </tbody>    
-          </Table>
-      
+      <div>
+        <Table>
+          <tbody>
+          <tr><td>{props.details.name}</td></tr>
+          <tr><td>{props.details.description}</td></tr>
+          <tr><td>{props.details.price}</td></tr>
+          </tbody>    
+        </Table>
+      </div>
+      <div>
+        <Form onSubmit= {handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Review</Form.Label>
+              <Form.Control type="review" name="Review" placeholder="Write Review Here" onChange= {handleChange} required= {true}/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" name="password" placeholder="Password" onChange= {handleChange} required= {true}/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" name="password" placeholder="Password" onChange= {handleChange} required= {true}/>
+          </Form.Group>
+
+          <Button variant="primary" type="submit" >
+             Submit
+          </Button>
+         </Form>
+      </div>
     </div>
   );
 };
+
+
+  
+
+  
+
 
 export default DetailTable;
