8b60bf0a3b09878c9cb551e47f0ccffe6f8e53a8
JESSE ARROYO-ROMERO <jesseromero310@gmail.com>
2021-11-30 16:05:09 -0600

    Added css styling

--- src/components/HeaderAndNav/HeaderAndNav.jsx
+++ src/components/HeaderAndNav/HeaderAndNav.jsx
@@ -3,81 +3,55 @@ import "./HeaderAndNav.css"
 import { Link } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { useNavigate } from "react-router";
+import useForm from "../LoginScreen/useForm";
 
 
+const HeaderAndNav = (props) =>{
+    const {formValues, handleChange, handleSubmit } = useForm(searchForProducts);
+    
+    let navigate= useNavigate();
 
-class HeaderAndNav extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            searchTerm:'' ,
-            logout:''         
-            
-        }
+    async function searchForProducts() { 
+        console.log(formValues)                  
+        props.productSearch(formValues.searchTerm)
+        navigate("../products")
     }
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    handleSubmit = async (event) => {
-        event.preventDefault();             
-        this.props.productSearch(this.state.searchTerm)
-    }
-
-    handleLogout = async (event) => {
-                 
-       this.props.logout()
-    }
-
-
-
-
-
-    render(){
+    
     return(
-        <div>
-            
-            
-            <div>
-                
+        <div>            
+            <h3>Welcome to Good Get</h3>
+            <div>                
                 <nav className='topNav'>
-                <Link to="/" >Home</Link>
+                <Link to="/" >Home</Link>                
+                <Link to="/products">Products</Link>
+                <Link to="/sellProducts" >Sell Products</Link>
                 <Link to="/userRegistration">User Registration</Link>
                 <Link to="/Profile">Profile</Link>
-                <Link to="/products">Products</Link>
-                <Link to="/sellProducts" >Sell Products</Link>                
                 <Link to="/ShoppingCart">Shopping Cart</Link>
                 <Link to="/login" >Login Here</Link>
-                <Link to="/login" onClick={() => {this.handleLogout()}}>Logout Here</Link>                
-                
-                <Form onSubmit= {this.handleSubmit} method= 'post'>
+                <Link to="/" onClick={() =>{props.logout() }}>Logout Here</Link>                
+                <Form onSubmit= {handleSubmit}>
                     <Form.Group>
                      <Form.Control
-                     type="text"
+                     type="searchTerm"
                      name="searchTerm"
-                     placeholder="Search for Product"
-                     value={this.state.searchTerm}
-                     onChange={this.handleChange}/>                  
+                     placeholder="Search for Product"                     
+                     onChange={handleChange}
+                     required= {true}/>                  
 
                     <Button variant="primary" type="submit">
                       Search
                     </Button>
                     </Form.Group>
                 </Form>
-                
-                
                 </nav>
-                
             </div>
-            
         </div>
-        
     )
-    }
 }
+
 
 
 export default HeaderAndNav
